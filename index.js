b81019a588c273e550ad8154d1b5801e39e3cfea
y.mikhin <y.mikhin@fun-box.ru>
2020-07-28 18:25:01 +0300

    Добавление обработки модификаторов типа «ключ-значение».

--- index.js
+++ index.js
@@ -8,19 +8,30 @@ const ELEMENT_SIGN = '__';
 const MOD_SIGN = '_';
 const STYLE_FILE_EXTENSION = '.scss';
 
-const watcher = chokidar.watch(COMPONENTS_FOLDER_PATH, { ignored: /^\./, persistent: true });
+const watcher = chokidar.watch(COMPONENTS_FOLDER_PATH, {
+  ignored: /^\./,
+  persistent: true,
+});
 
 function isEmpty(filePath) {
   return fs.readdirSync(filePath).length === 0;
 }
 
 function isElementFolder(filePath) {
-  return path.basename(filePath).includes(ELEMENT_SIGN);
+  return path.basename(filePath)
+    .includes(ELEMENT_SIGN);
 }
 
 function isModFolder(filePath) {
-  return path.basename(filePath).includes(MOD_SIGN)
+  return path.basename(filePath)
+    .includes(MOD_SIGN)
     && !isElementFolder(filePath);
+}
+
+function isBoolModFolder(filePath) {
+  return filePath.split('/')
+    .filter((folder) => folder.includes(MOD_SIGN) && !folder.includes(ELEMENT_SIGN))
+    .length === 1;
 }
 
 function getBlockName(filePath) {
@@ -36,6 +47,12 @@ function getElementName(filePath) {
     .replace(/\/.{1,}/, '');
 }
 
+function getBoolModName(filePath) {
+  const modFolders = filePath.split('/')
+    .filter((folder) => folder.includes(MOD_SIGN) && !folder.includes(ELEMENT_SIGN));
+  return modFolders[0];
+}
+
 function getModName(filePath) {
   return path.basename(filePath);
 }
@@ -44,12 +61,32 @@ function getNewElementContent(blockName, elementName) {
   return `.${blockName}${elementName} {\n  \n}`;
 }
 
-function getNewElementModContent(blockName, elementName, modName) {
+function getNewElementBoolModContent(blockName, elementName, modName) {
   return `.${blockName}${elementName}${modName} {\n  \n}`;
 }
 
-function getNewBlockModContent(blockName, modName) {
+function getNewElementModContent(blockName, elementName, boolModName, modName) {
+  return `.${blockName}${elementName}${boolModName}${modName} {\n  \n}`;
+}
+
+function getNewBlockBoolModContent(blockName, modName) {
   return `.${blockName}${modName} {\n  \n}`;
+}
+
+function getNewBlockModContent(blockName, boolModName, modName) {
+  return `.${blockName}${boolModName}${modName} {\n  \n}`;
+}
+
+function updateIndex(filePath, newFilePath) {
+  const indexPath = `./${COMPONENTS_FOLDER_PATH}${getBlockName(filePath)}/index.js`;
+  const indexContent = fs.readFileSync(indexPath, { encoding: 'utf-8' });
+  const relativeNewFilePath = `.${newFilePath
+    .replace(COMPONENTS_FOLDER_PATH, '')
+    .replace(getBlockName(filePath), '')}`;
+
+  if (indexContent.includes(relativeNewFilePath)) return undefined;
+
+  return 1;
 }
 
 function createElementFile(filePath) {
@@ -59,25 +96,55 @@ function createElementFile(filePath) {
   const newFilePath = `${filePath}/${blockName}${elementName}${STYLE_FILE_EXTENSION}`;
 
   fs.writeFileSync(newFilePath, getNewElementContent(blockName, elementName));
+
+  updateIndex(filePath, newFilePath);
 }
 
-function createElementModFile(filePath) {
+function createElementBoolModFile(filePath) {
   const blockName = getBlockName(filePath);
   const elementName = getElementName(filePath);
   const modName = getModName(filePath);
 
   const newFilePath = `${filePath}/${blockName}${elementName}${modName}${STYLE_FILE_EXTENSION}`;
 
-  fs.writeFileSync(newFilePath, getNewElementModContent(blockName, elementName, modName));
+  fs.writeFileSync(newFilePath, getNewElementBoolModContent(blockName, elementName, modName));
 }
 
-function createBlockModFile(filePath) {
+function createElementModFile(filePath) {
+  const blockName = getBlockName(filePath);
+  const elementName = getElementName(filePath);
+  const boolModName = getBoolModName(filePath);
+  const modName = getModName(filePath);
+
+  const newFilePath = `${filePath}/${blockName}${elementName}${boolModName}${modName}${STYLE_FILE_EXTENSION}`;
+
+  fs.writeFileSync(newFilePath,
+    getNewElementModContent(blockName, elementName, boolModName, modName));
+}
+
+function createBlockBoolModFile(filePath) {
   const blockName = getBlockName(filePath);
   const modName = getModName(filePath);
 
   const newFilePath = `${filePath}/${blockName}${modName}${STYLE_FILE_EXTENSION}`;
 
-  fs.writeFileSync(newFilePath, getNewBlockModContent(blockName, modName));
+  fs.writeFileSync(newFilePath, getNewBlockBoolModContent(blockName, modName));
+}
+
+function createBlockModFile(filePath) {
+  const blockName = getBlockName(filePath);
+  const boolModName = getBoolModName(filePath);
+  const modName = getModName(filePath);
+
+  const newFilePath = `${filePath}/${blockName}${boolModName}${modName}${STYLE_FILE_EXTENSION}`;
+
+  fs.writeFileSync(newFilePath, getNewBlockModContent(blockName, boolModName, modName));
+}
+
+function createBoolModFile(filePath) {
+  return filePath.includes(ELEMENT_SIGN)
+    ? createElementBoolModFile(filePath)
+    : createBlockBoolModFile(filePath);
 }
 
 function createModFile(filePath) {
@@ -87,17 +154,18 @@ function createModFile(filePath) {
 }
 
 function onDirAdd(filePath) {
-  if (isEmpty((filePath))) {
+  if (isEmpty(filePath)) {
     if (isElementFolder(filePath)) createElementFile(filePath);
-    if (isModFolder(filePath)) createModFile(filePath);
+    if (isModFolder(filePath) && isBoolModFolder(filePath)) createBoolModFile(filePath);
+    if (isModFolder(filePath) && !isBoolModFolder(filePath)) createModFile(filePath);
   }
 }
 
 function onReady() {
-  console.log('Watching is activated');
-
   watcher
     .on('addDir', onDirAdd);
 }
 
-module.exports = () => watcher.on('ready', onReady);
+onReady();
+
+// module.exports = () => watcher.on('ready', onReady);
