68958176c7dd5c9272335483b1b5fbcf0e803cf2
y.mikhin <y.mikhin@fun-box.ru>
2021-04-10 15:23:29 +0300

    Упразднение модуля remove-file-path-from-index-file.js.

--- src/handlers/delete-directory-handler.js
+++ src/handlers/delete-directory-handler.js
@@ -1,7 +1,35 @@
-const removeFilePathFromIndexFile = require('../index-file/remove-file-path-from-index-file');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
 
-function onFileDelete(bemFilePath) {
-  removeFilePathFromIndexFile(bemFilePath);
+const getBlockName = require('../bem/get-block-name');
+const getIndexFilePath = require('./../index-file/get-index-file-path');
+const getIndexFileContent = require('./../index-file/get-index-file-content');
+const { COMPONENTS_FOLDER_PATH, STYLE_FILE_EXTENSION } = require('./../constants');
+const fileSystemPathSeparator = require('../file-system-path-separator');
+
+const { EOL } = os;
+
+function onFileDelete(filePath) {
+  const indexFilePath = getIndexFilePath(filePath);
+  const indexFileContent = getIndexFileContent(indexFilePath);
+
+  const fileSepRegExp = new RegExp(`${fileSystemPathSeparator}`, 'g');
+  const selector = filePath
+    .replace(COMPONENTS_FOLDER_PATH, '')
+    .replace(fileSepRegExp, '');
+  const deletingFilePath = `${selector}${STYLE_FILE_EXTENSION}`;
+
+  const newIndexFileContent = `${indexFileContent
+    .split(EOL)
+    .filter((string) => !string.includes(deletingFilePath))
+    .join(EOL)}${EOL}`;
+
+  try {
+    fs.writeFileSync(indexFilePath, newIndexFileContent);
+  } catch (e) {
+    // ;
+  }
 }
 
 module.exports = onFileDelete;
